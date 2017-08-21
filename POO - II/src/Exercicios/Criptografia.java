9db373226ee21e0f1ed2d2b6bc6c250d43f48c78
Gabrielkoyamao <30911411+Gabrielkoyamao@users.noreply.github.com>
2017-08-20 22:05:33 -0300

    Update Criptografia.java

--- POO - II/src/Exercicios/Criptografia.java
+++ POO - II/src/Exercicios/Criptografia.java
@@ -3,32 +3,33 @@ import java.util.Scanner;
 
 public class Criptografia {
 
-	void demonio(){
+	void criptografa(){
 	
-	int senha;
+	int senha, senha1,senha2,senha3,senha4;
 	char[] separa;
-		
+	
+	 
+			
 	Scanner leitor = new Scanner(System.in);
 	
+	// ler
 	System.out.println("Digite os numeros a serem criptografados: ");
 	 senha = leitor.nextInt();
 	
-	//Separar os numeros;                            !404!
+	//separa
+	 senha1 = senha/1000;
+	 senha2 = (senha%1000)/100;
+	 senha3 = (senha%100)/10;
+	 senha4 = senha%10;
 	 
-	 separa = String.valueOf(senha).toCharArray();
+	 //criptografa
+	 senha1 = (senha1 + 7)%10;
+	 senha2 = (senha2 + 7)%10;
+	 senha3 = (senha3 + 7)%10;
+	 senha4 = (senha4 + 7)%10;
 	 
+	 //senha = senha3 + senha4 + senha1 + senha2;
+	 
+	 System.out.printf("%d%d%d%d",senha3, senha4, senha1, senha2);
+	  
 	}
-	
-}
-
-/*
- dados1 = senha/1000;
-	dados2 = senha/100;
-	dados3 = senha/10;
-	dados4 = senha/1;
-	
-	System.out.println(+dados1);
-	System.out.println(+dados2);
-	System.out.println(+dados3);
-	System.out.println(+dados4);
- */
